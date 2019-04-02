642d1b053fb72f9ebeea562f447eabec0b971a09
Tom Holmes <tholmes59@gmail.com>
2019-04-02 22:44:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < Sinatra::Base
+  
+  set :views, Proc.new { File.join(root, "../views/")
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
